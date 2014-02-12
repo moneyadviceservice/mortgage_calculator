97074c8a8baed5eb2442f2fe2b9944bd9923b867
Phil Lee <asmega@ph-lee.com>
2014-02-12 16:15:15 +0000

    move tests into MortgageCalculator module

--- spec/models/affordability_spec.rb
+++ spec/models/affordability_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
-describe MortgageCalculator::Affordability do
-  context 'when the user is a sole buyer' do
-    let(:person1){ MortgageCalculator::Affordability::Person.new({ annual_income: 100000, extra_income: 10000 }) }
+module MortgageCalculator
+  describe Affordability do
+    context 'when the user is a sole buyer' do
+      let(:person1){ Affordability::Person.new({ annual_income: 100000, extra_income: 10000 }) }
 
-    subject{ described_class.new([person1], 1000) }
+      subject{ described_class.new([person1], 1000) }
 
-    its(:total_income){ should == 110000 }
-    its(:can_borrow){ should == 441000.00 }
-    its(:number_of_applicants){ should == 1 }
-  end
+      its(:total_income){ should == 110000 }
+      its(:can_borrow){ should == 441000.00 }
+      its(:number_of_applicants){ should == 1 }
+    end
 
-  context 'when there are multiple applicants' do
-    let(:person1){ MortgageCalculator::Affordability::Person.new({ annual_income: 100000, extra_income: 10000 }) }
-    let(:person2){ MortgageCalculator::Affordability::Person.new({ annual_income: 50000, extra_income: 5000 }) }
+    context 'when there are multiple applicants' do
+      let(:person1){ Affordability::Person.new({ annual_income: 100000, extra_income: 10000 }) }
+      let(:person2){ Affordability::Person.new({ annual_income: 50000, extra_income: 5000 }) }
 
-    subject{ described_class.new([person1, person2], 1000) }
+      subject{ described_class.new([person1, person2], 1000) }
 
-    its(:total_income){ should == 165000 }
-    its(:can_borrow){ should == 688500.00 }
-    its(:number_of_applicants){ should == 2 }
+      its(:total_income){ should == 165000 }
+      its(:can_borrow){ should == 688500.00 }
+      its(:number_of_applicants){ should == 2 }
+    end
   end
 end
