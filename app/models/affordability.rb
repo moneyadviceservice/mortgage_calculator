ef736b09c66b805c2129a1007be7ccdd46da7579
Jared Fraser <dev@jsf.io>
2014-02-13 13:31:42 +0000

    Moved income multiplier into a separate method

--- app/models/affordability.rb
+++ app/models/affordability.rb
@@ -15,11 +15,17 @@ module MortgageCalculator
     end
 
     def can_borrow
-      @can_borrow ||= ((total_income) - (12 * monthly_debt)) * INCOME_MULTIPLIER
+      @can_borrow ||= ((total_income) - (12 * monthly_debt)) * income_multiplier
     end
 
     def number_of_applicants
       people.length
+    end
+
+  private
+
+    def income_multiplier
+      INCOME_MULTIPLIER
     end
   end
 end
