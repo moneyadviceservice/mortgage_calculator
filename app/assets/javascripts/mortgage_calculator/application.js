a96a812bb54c3342c4efe620ef6ffe78904cf9c9
David Kewal <david.kewal@gmail.com>
2014-02-13 14:18:56 +0000

    set up affordability calculator state view, also included ui router library for state management

--- app/assets/javascripts/mortgage_calculator/application.js
+++ app/assets/javascripts/mortgage_calculator/application.js
@@ -14,7 +14,11 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-resource
+//= require angular-ui-router
 //= require underscore
 //= require mortgage_calculator/mortgage_calculator_app
-//= require_tree ./angular
-//= require_tree .
+//= require_tree ./angular/services
+//= require_tree ./angular/factories
+//= require_tree ./angular/directives
+//= require_tree ./angular/controllers
+//= require_self
--- app/assets/javascripts/mortgage_calculator/mortgage_calculator_app.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-window.App = angular.module('mortgageCalculatorApp', []);
