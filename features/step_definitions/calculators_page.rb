f614b200c57739ee7cca0bf0794a95cc7a04b990
Phil Lee <asmega@ph-lee.com>
2014-02-25 15:27:22 +0000

    remove unused step definitions

--- features/step_definitions/calculators_page.rb
+++ features/step_definitions/calculators_page.rb
@@ -6,10 +6,6 @@ Then /^I should see the Affordability calculator$/ do
   expect(page).to have_content('Find out how much you can borrow')
 end
 
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
-  fill_in field, :with => value
-end
-
 Then(/^I see "(.*?)"$/) do |content|
   expect(page).to have_content(content)
 end
@@ -18,15 +14,8 @@ When(/^I select (\d+) people$/) do |number|
   select(number, from: 'numberOfPeople')
 end
 
-Then(/^I see "(.*?)" text inputs$/) do |count|
-  all("input[type=text]").length.should == count.to_i
-end
-
 When(/^I submit the details$/) do
   find("input[type=submit]").click
-end
-
-Then(/^I see "(.*?)" select dropdowns$/) do |count|
 end
 
 Then(/^I can change the number of applicants$/) do
