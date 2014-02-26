22362301155e6ce5d8ba400f40190484cd3cb843
David Kewal <david.kewal@gmail.com>
2014-02-26 15:24:38 +0000

    build script for go

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+set -e -x
+
+export RAILS_ENV=build
+export BUNDLE_WITHOUT="development:test"
+
+CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
+CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
+
+bundle install
+
+rake app:gem:build
