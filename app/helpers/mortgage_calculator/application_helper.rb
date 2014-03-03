37a6a3e20ae67380f0a3e5bea33c430f3e486c2d
Phil Lee <asmega@ph-lee.com>
2014-03-03 15:00:29 +0000

    memoize zendesk config

--- app/helpers/mortgage_calculator/application_helper.rb
+++ app/helpers/mortgage_calculator/application_helper.rb
@@ -4,8 +4,10 @@ module MortgageCalculator
     include Mas::Feedback::ApplicationHelper
 
     def zendesk_stamp_duty_config
+      return @zendesk_stamp_duty_config if @zendesk_stamp_duty_config
+
       contents = File.open(MortgageCalculator::Engine.root.join('config','zendesk.yml')).read
-      YAML.load(contents).with_indifferent_access[:stamp_duty]
+      @zendesk_stamp_duty_config = YAML.load(contents).with_indifferent_access[:stamp_duty]
     end
   end
 end
