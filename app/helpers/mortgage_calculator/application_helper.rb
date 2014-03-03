f460b2a0b66ae1806ea06fd5cadaf88a8dd1c037
Phil Lee <asmega@ph-lee.com>
2014-03-03 15:51:11 +0000

    refactor method

--- app/helpers/mortgage_calculator/application_helper.rb
+++ app/helpers/mortgage_calculator/application_helper.rb
@@ -4,9 +4,7 @@ module MortgageCalculator
     include Mas::Feedback::ApplicationHelper
 
     def zendesk_stamp_duty_config
-      return @zendesk_stamp_duty_config if @zendesk_stamp_duty_config
-
-      @zendesk_stamp_duty_config = MortgageCalculator.feedback_config[:stamp_duty]
+      @zendesk_stamp_duty_config ||= MortgageCalculator.feedback_config[:stamp_duty]
     end
   end
 end
