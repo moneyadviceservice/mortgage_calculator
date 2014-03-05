d8bb15adc50ff22d5925927b7c321cd35df22d76
David Kewal <david.kewal@gmail.com>
2014-03-05 15:44:29 +0000

    spec for currency directive

---
+++ spec/javascripts/unit/directives/currency.js
@@ -0,0 +1,45 @@
+'use strict';
+
+describe("Directives: Currency", function() {
+
+  var element;
+  var $scope;
+  var form;
+
+  beforeEach(module('mortgageCalculatorApp'));
+
+  beforeEach(inject(function($compile, $rootScope){
+    $scope = $rootScope;
+    element = angular.element(
+      '<form name="form">' +
+        '<input type="text" name="annualIncome" ng-model="annualIncome" currency>' +
+      '</form>'
+    );
+    $scope.model = { annualIncome: null};
+    $compile(element)($scope);
+    $scope.$digest();
+    form = $scope.form;
+  }));
+
+  it('should be valid initially', function() {
+      expect(form.annualIncome.$valid).toBe(true);
+  });
+
+  it('should be invalid when user enters letters', function(){
+    form.annualIncome.$setViewValue('sas');
+    expect(form.annualIncome.$valid).toBe(false);
+  });
+
+  it('should contain invalidCurrency when user enters letters', function(){
+    form.annualIncome.$setViewValue('asas');
+    expect(form.$error.invalidCurrency).toBeDefined();
+  });
+
+  it('should not contain invalidCurrency when a user enters numbers', function(){
+    form.annualIncome.$setViewValue('50000');
+    expect(form.$error.invalidCurrency).not.toBeDefined();
+  });
+
+});
+
+
