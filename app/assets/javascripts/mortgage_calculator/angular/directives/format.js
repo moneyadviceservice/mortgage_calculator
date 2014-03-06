bb4356f0828ed79093f130489a7c5702902bd52c
David Kewal <david.kewal@gmail.com>
2014-03-06 12:30:29 +0000

    format input values as the user types. Consolidate converting user inputs to integers and formatting into one directive

---
+++ app/assets/javascripts/mortgage_calculator/angular/directives/format.js
@@ -0,0 +1,33 @@
+angular.module('mortgageCalculatorApp')
+
+  .directive('format', ['$filter', function($filter){
+    // requires an isloated model
+    return {
+
+     restrict: 'A',
+
+     require: 'ngModel',
+     link: function(scope, ele, attrs, ctrl){
+
+
+
+        ctrl.$formatters.unshift(function (a) {
+          return $filter(attrs.format)(ctrl.$modelValue);
+        });
+
+
+        ctrl.$parsers.unshift(function (viewValue) {
+
+          if (viewValue) {
+            var plainNumber = viewValue.replace(/[^\d|\-+|\.+]/g, '');
+            ele.val($filter('number')(plainNumber));
+            return parseInt(plainNumber.replace(/,/g, ''), 10);
+          }
+
+        });
+
+
+     }
+    }
+
+  }]);
