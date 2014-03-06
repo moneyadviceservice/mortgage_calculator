e0e4a10df7e7cd5cd0851713ae48f2efd28f6499
David Kewal <david.kewal@gmail.com>
2014-03-06 12:47:21 +0000

    create page object for affordability calculator page

---
+++ features/support/ui/pages/affordabiity.rb
@@ -0,0 +1,18 @@
+module UI
+  module Pages
+    class Affordability < SitePrism::Page
+      include DefaultLocale
+
+      set_url "/{locale}/mortgage_calculator/affordability"
+
+      element :annual_income, "input[name='affordability[people_attributes][0][annual_income]']"
+      element :extra_income, "input[name='affordability[people_attributes][0][extra_income]']"
+      element :person_two_annual_income, "input[name='affordability[people_attributes][1][annual_income]']"
+      element :person_two_extra_income, "input[name='affordability[people_attributes][1][extra_income]']"
+      element :monthly_debt, "input[name='affordability[monthly_debt]']"
+      element :h1, "h1"
+      element :results, "p[class='results']"
+    end
+  end
+end
+
