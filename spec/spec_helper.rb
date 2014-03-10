c5bce0bec61bcdab503d00f99cec7c0f236e9156
Phil Lee <asmega@ph-lee.com>
2014-03-10 13:35:36 +0000

    add and configure simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'mas/development_dependencies/rspec/spec_helper'
-
 
