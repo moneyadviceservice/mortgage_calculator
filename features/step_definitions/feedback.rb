7420c3bbae86f8b9da1fbab791bb9db9c7dd22bc
Phil Lee <asmega@ph-lee.com>
2014-03-13 12:51:17 +0000

    fix feedback tests

--- features/step_definitions/feedback.rb
+++ features/step_definitions/feedback.rb
@@ -1,12 +1,12 @@
 Then(/^I should see the stamp duty feedback tab$/) do
-  page.has_link?("https://mastest.zendesk.com/account/dropboxes/20289328").should be_true
+  page.has_link?("Feedback").should be_true
 end
 
 Then(/^I should see the affordability feedback tab$/) do
-  page.has_link?("https://mastest.zendesk.com/account/dropboxes/20293976").should be_true
+  page.has_link?("Feedback").should be_true
 end
 
 Then(/^I should see the repayment feedback tab$/) do
-  page.has_link?("https://mastest.zendesk.com/account/dropboxes/20283343").should be_true
+  page.has_link?("Feedback").should be_true
 end
 
