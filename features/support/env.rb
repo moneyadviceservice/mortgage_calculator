f14faf744162c490463a1f791f50fdfa666e4bac
Phil Lee <asmega@ph-lee.com>
2014-03-25 15:12:15 +0000

    make site prism wait

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,3 +3,7 @@ ENV["RAILS_ROOT"] ||= File.dirname(__FILE__) + "../../../spec/dummy"
 
 require 'mas/development_dependencies/cucumber/env'
 
+SitePrism.configure do |config|
+  config.use_implicit_waits = true
+end
+
