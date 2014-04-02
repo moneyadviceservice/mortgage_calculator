79265b4680f8ac0955a0b092b3c3cf8e412d354e
David Kewal <david.kewal@gmail.com>
2014-04-02 17:46:47 +0100

    update stamp duty page object to reflect view

--- features/support/ui/pages/stamp_duty.rb
+++ features/support/ui/pages/stamp_duty.rb
@@ -5,10 +5,16 @@ module UI
 
       set_url "/{locale}/mortgage_calculator/stamp_duty"
 
-      element :property_price, "input[name='stamp_duty[price]']"
       element :h2, "h2"
+      element :property_price, "form.new_stamp_duty input[name='stamp_duty[price]']"
+      element :property_price_step_two, "form.step_two input[name='stamp_duty[price]']"
+
+      element :next, "form.new_repayment input[type=submit]"
+
       element :results, "p[class='results']"
-      element :submit, "input[type=submit]"
+      element :results_text, "p[class='results-text']"
+
+      element :recalculate, "form.step_two input[type=submit]"
     end
   end
 end
