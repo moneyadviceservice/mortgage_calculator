a54ddb166905b7773ae08c079c67bff342c52788
Phil Lee <asmega@ph-lee.com>
2014-04-08 09:53:45 +0100

    add analytics event type to directive

--- app/assets/javascripts/mortgage_calculator/angular/directives/analytics.js
+++ app/assets/javascripts/mortgage_calculator/angular/directives/analytics.js
@@ -1,11 +1,14 @@
 App.directive('analytics', function () {
   var linker = function (scope, element, attrs, ctrl) {
     element.on('click', function(){
+      type = attrs['analyticsOn'];
       category = attrs['analyticsCategory'];
       action = attrs['analyticsAction'];
       label = attrs['analyticsLabel'];
 
-      _gaq.push(['_trackEvent',category,action,label]);
+      if (type == 'click'){
+        _gaq.push(['_trackEvent',category,action,label]);
+      }
     });
   };
 
