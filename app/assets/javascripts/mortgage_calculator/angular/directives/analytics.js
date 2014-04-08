043505eae94ab6cd24b48c47b7b792dbf4fb6099
Phil Lee <asmega@ph-lee.com>
2014-04-08 13:19:18 +0100

    refactor js. tidy up leaky variables

--- app/assets/javascripts/mortgage_calculator/angular/directives/analytics.js
+++ app/assets/javascripts/mortgage_calculator/angular/directives/analytics.js
@@ -1,22 +1,17 @@
 App.directive('analytics', function () {
   var linker = function (scope, element, attrs, ctrl) {
-    element.on('click', function(){
-      type = attrs['analyticsOn'];
-      category = attrs['analyticsCategory'];
-      action = attrs['analyticsAction'];
-      label = attrs['analyticsLabel'];
+    var type = attrs['analyticsOn'],
+        category = attrs['analyticsCategory'],
+        action = attrs['analyticsAction'],
+        label = attrs['analyticsLabel'];
 
+    element.on('click', function(){
       if (type == 'click'){
         _gaq.push(['_trackEvent',category,action,label]);
       }
     });
 
     element.on('change', function(){
-      type = attrs['analyticsOn'];
-      category = attrs['analyticsCategory'];
-      action = attrs['analyticsAction'];
-      label = attrs['analyticsLabel'];
-
       if (type == 'change'){
         if (action == 'Refinement') {
           var refined = element.attr('refined');
