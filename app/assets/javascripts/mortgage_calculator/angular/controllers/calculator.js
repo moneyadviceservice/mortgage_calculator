c05899e5d66c77471f3621c96b5d303715ec92e9
Phil Lee <asmega@ph-lee.com>
2014-04-15 11:21:25 +0100

    iframe resizing works on last page and on load

--- app/assets/javascripts/mortgage_calculator/angular/controllers/calculator.js
+++ app/assets/javascripts/mortgage_calculator/angular/controllers/calculator.js
@@ -39,7 +39,6 @@ App.controller('CalculatorCtrl', ['$scope', 'Affordability', 'StampDuty', 'Repay
       $scope.viewInterestRepayments = true;
     };
 
-
     // All Calculator Helpers
     $scope.preventFormSubmission = function($event) {
       $event.preventDefault();
@@ -56,15 +55,22 @@ App.controller('CalculatorCtrl', ['$scope', 'Affordability', 'StampDuty', 'Repay
 
       // we have no hooks for when angular-wizard has completed its navigation, so we have to wait for a tick
       setTimeout(function() {
-        var minPageHeight = 250; // px
-        if (window.postMessage) {
-          var height = document.body.offsetHeight || document.documentElement.offsetHeight;
-          height = Math.max(height, minPageHeight);
-          window.parent.postMessage('MASRESIZE-' + height, '*');
-        }
-
+        $scope.resizeIframe();
         $('input[name="repayment[price]"]:visible').focus();
         $('input[name="stamp_duty[price]"]:visible').focus();
       });
     };
+
+    $scope.resizeIframe = function() {
+      var minPageHeight = 250; // px
+      if (window.postMessage) {
+        var height = document.documentElement.offsetHeight;
+        height = Math.max(height, minPageHeight);
+        window.parent.postMessage('MASRESIZE-' + height, '*');
+      }
+    };
+
+    setTimeout(function() {
+      $scope.resizeIframe();
+    });
   }]);
