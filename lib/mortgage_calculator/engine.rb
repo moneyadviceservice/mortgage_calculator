7bf5d70fd7ca4748489f1fc56fec20e9b99b986e
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-05-15 10:48:56 +0100

    Rewiring bower to use frontend-assets repo

--- lib/mortgage_calculator/engine.rb
+++ lib/mortgage_calculator/engine.rb
@@ -34,11 +34,11 @@ module MortgageCalculator
     end
 
     initializer :append_frontend_assets do |app|
-      app.config.assets.paths << Engine.root.join("vendor", "assets", "components")
-      app.config.assets.paths << Engine.root.join("vendor", "assets", "components", "mortgage_calculator", "frontend", "app", "assets", "stylesheets")
-      app.config.assets.paths << Engine.root.join("vendor", "assets", "components", "mortgage_calculator", "frontend", "vendor", "assets", "fonts")
-      app.config.sass.load_paths << Engine.root.join("vendor", "assets", "components", "mortgage_calculator", "frontend", "app", "assets", "stylesheets")
-      app.config.sass.load_paths << Engine.root.join("vendor", "assets", "components", "mortgage_calculator", "frontend", "app", "assets", "stylesheets", "lib")
+      app.config.assets.paths << Engine.root.join("vendor", "assets", "bower_components")
+      app.config.assets.paths << Engine.root.join("vendor", "assets", "bower_components", "mortgage_calculator", "frontend", "app", "assets", "stylesheets")
+      app.config.assets.paths << Engine.root.join("vendor", "assets", "bower_components", "mortgage_calculator", "frontend", "vendor", "assets", "fonts")
+      app.config.sass.load_paths << Engine.root.join("vendor", "assets", "bower_components", "mortgage_calculator", "frontend", "app", "assets", "stylesheets")
+      app.config.sass.load_paths << Engine.root.join("vendor", "assets", "bower_components", "mortgage_calculator", "frontend", "app", "assets", "stylesheets", "lib")
     end
   end
 end
