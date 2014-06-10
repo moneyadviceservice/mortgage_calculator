abe942669f49372e0624a73130351fb3971a1c80
Phil Lee <asmega@ph-lee.com>
2014-06-10 12:55:00 +0100

    change borrowing slider id

--- features/support/ui/pages/affordabiity.rb
+++ features/support/ui/pages/affordabiity.rb
@@ -32,7 +32,7 @@ module UI
       element :interest_rate, "input[name='affordability[interest_rate]']"
       element :lifestyle, "input[name='affordability[lifestyle_costs]']"
 
-      section :borrowing_slider, UI::Sections::Slider, "#borrowing-amount-slider"
+      section :borrowing_slider, UI::Sections::Slider, "#slider-borrowing"
       section :interest_rate_slider, UI::Sections::Slider, "#slider-interest-rate"
       section :lifestyle_slider, UI::Sections::Slider, "#slider-lifestyle"
 
