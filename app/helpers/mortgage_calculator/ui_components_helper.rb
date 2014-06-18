a8af53cffdc1a5a7f233b4bd4cd66222919cc125
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-06-18 11:52:17 +0100

    Restoring inset blocko

--- app/helpers/mortgage_calculator/ui_components_helper.rb
+++ app/helpers/mortgage_calculator/ui_components_helper.rb
@@ -1,7 +1,7 @@
 module MortgageCalculator
    module UiComponentsHelper
       def inset_block(text)
-        render 'mortgage_calculator/affordabilities/inset_block', text
+        render 'mortgage_calculator/affordabilities/inset_block', text: text
       end
 
       def field_tooltip(args)
