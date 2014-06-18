4f05709d72a609dd1d4635d77e7b07fa851b57f0
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-06-18 11:42:18 +0100

    Moving field tooltip into own view helper

--- app/helpers/mortgage_calculator/ui_components_helper.rb
+++ app/helpers/mortgage_calculator/ui_components_helper.rb
@@ -1,7 +1,11 @@
 module MortgageCalculator
    module UiComponentsHelper
-      def inset_block(args)
-        render 'mortgage_calculator/affordabilities/inset-block', args
+      def inset_block(text)
+        render 'mortgage_calculator/affordabilities/inset_block', text
+      end
+
+      def field_tooltip(args)
+        render 'mortgage_calculator/affordabilities/field_tooltip', args
       end
    end
 end
