ab1156cd1f1098c8ea07e5ee1cf60f3e34232125
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-06-23 11:03:29 +0100

    Initial docker directive

---
+++ app/assets/javascripts/mortgage_calculator/angular/directives/docker.js
@@ -0,0 +1,12 @@
+'use strict';
+
+App.directive('ngDocker', function() {
+  return {
+    restrict: 'A',
+    controller: [
+      '$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse) {
+        $element.waypoint('sticky');
+      }
+    ]
+  };
+});
