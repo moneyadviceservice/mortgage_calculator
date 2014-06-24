78c6dab19fb4e9457c432242a897bd54cb82a6c3
Gareth Visagie <gareth@gjvis.com>
2014-06-24 14:52:48 +0100

    Remove unnecessary vars in Person initializer

--- app/models/mortgage_calculator/person.rb
+++ app/models/mortgage_calculator/person.rb
@@ -19,13 +19,9 @@ module MortgageCalculator
     currency_inputs :annual_income, :extra_income, :monthly_net_income
 
     def initialize(options = {})
-      annual_income = options[:annual_income].presence || 0
-      extra_income = options[:extra_income].presence || 0
-      monthly_net_income = options[:monthly_net_income].presence || 0
-
-      self.annual_income = annual_income
-      self.extra_income = extra_income
-      self.monthly_net_income = monthly_net_income
+      self.annual_income = options[:annual_income].presence || 0
+      self.extra_income = options[:extra_income].presence || 0
+      self.monthly_net_income = options[:monthly_net_income].presence || 0
     end
 
     def persisted?
