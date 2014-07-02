fa1ed0300545ae01d69cb61e362f259615a503b8
Phil Lee <asmega@ph-lee.com>
2014-07-02 10:22:32 +0100

    fix brittle test

--- spec/models/person_one_spec.rb
+++ spec/models/person_one_spec.rb
@@ -30,7 +30,7 @@ describe MortgageCalculator::PersonOne do
       end
 
       it 'adds validation for required fields' do
-        expect(subject.errors.messages.values.flatten).to include("Your monthly take-home pay can't be blank")
+        expect(subject.errors.messages.values.flatten).to include("Your monthly take-home pay can't be blank. Please enter Your monthly take-home pay.")
         expect(subject.errors.messages.values.flatten).to include(I18n.t("affordability.activemodel.errors.mortgage_calculator/person_one.base.positive_total_income"))
       end
     end
