69361da3ad1d905a0cef1eefac6d186e5133fffe
Phil Lee <asmega@ph-lee.com>
2014-07-02 17:28:15 +0100

    validation messages now procs

--- app/models/mortgage_calculator/person_one.rb
+++ app/models/mortgage_calculator/person_one.rb
@@ -1,6 +1,6 @@
 module MortgageCalculator
   class PersonOne < Person
-    validates :annual_income, numericality: { greater_than: 0, message: I18n.t("affordability.activemodel.errors.messages.blank", attribute: self.human_attribute_name(:annual_income).downcase) }
-    validates :monthly_net_income, numericality: { greater_than: 0, message: I18n.t("affordability.activemodel.errors.messages.blank", attribute: self.human_attribute_name(:monthly_net_income).downcase) }
+    validates :annual_income, numericality: { greater_than: 0, message: Proc.new { I18n.t("affordability.activemodel.errors.messages.blank", attribute: self.human_attribute_name(:annual_income).downcase) } }
+    validates :monthly_net_income, numericality: { greater_than: 0, message: Proc.new { I18n.t("affordability.activemodel.errors.messages.blank", attribute: self.human_attribute_name(:monthly_net_income).downcase) } }
   end
 end
--- app/models/mortgage_calculator/person_other.rb
+++ app/models/mortgage_calculator/person_other.rb
@@ -1,7 +1,7 @@
 module MortgageCalculator
   class PersonOther < Person
-    validates :annual_income, numericality: {greater_than: 0, message: I18n.t("affordability.activemodel.errors.messages.greater_than", attribute: self.human_attribute_name(:annual_income).downcase)}, if: "monthly_net_income > 0"
-    validates :monthly_net_income, numericality: {greater_than: 0, message: I18n.t("affordability.activemodel.errors.messages.greater_than", attribute: self.human_attribute_name(:monthly_net_income).downcase)}, if: "annual_income > 0"
+    validates :annual_income, numericality: {greater_than: 0, message: Proc.new { I18n.t("affordability.activemodel.errors.messages.greater_than", attribute: self.human_attribute_name(:annual_income).downcase)} }, if: "monthly_net_income > 0"
+    validates :monthly_net_income, numericality: {greater_than: 0, message: Proc.new{ I18n.t("affordability.activemodel.errors.messages.greater_than", attribute: self.human_attribute_name(:monthly_net_income).downcase)} }, if: "annual_income > 0"
 
     def initialize(options = {})
       self.annual_income = options[:annual_income].presence || 0
