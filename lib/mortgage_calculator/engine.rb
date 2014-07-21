cdfddc3158876f9501cd20df45438445eeff0ab3
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-07-21 15:12:17 +0100

    Adding to precompile

--- lib/mortgage_calculator/engine.rb
+++ lib/mortgage_calculator/engine.rb
@@ -39,6 +39,7 @@ module MortgageCalculator
       app.config.assets.precompile += ['mortgage_calculator_enhanced_fixed.css']
       app.config.assets.precompile += ['mortgage_calculator_enhanced_responsive.css']
       app.config.assets.precompile += ['mortgage_calculator/frontend-assets/stylesheets/font_base64.css']
+      app.config.assets.precompile += ['mortgage_calculator_print.css']
 
       app.config.assets.precompile << 'd3.js'
       app.config.assets.precompile << 'r2d3.js'
