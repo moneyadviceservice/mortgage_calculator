d554571ee61663d24e012905fc6f1795aaa899aa
Phil Lee <asmega@ph-lee.com>
2014-07-22 11:41:39 +0100

    remove custom inset_block helper method
    this will now use the helper method implemented in dough instead

--- app/helpers/mortgage_calculator/ui_components_helper.rb
+++ app/helpers/mortgage_calculator/ui_components_helper.rb
@@ -1,11 +1,7 @@
 module MortgageCalculator
    module UiComponentsHelper
-      def inset_block(text)
-        render 'mortgage_calculator/affordabilities/inset_block', text: text
-      end
-
-      def field_tooltip(*args)
-        render 'mortgage_calculator/affordabilities/field_tooltip', *args
-      end
+     def field_tooltip(*args)
+       render 'mortgage_calculator/affordabilities/field_tooltip', *args
+     end
    end
 end
