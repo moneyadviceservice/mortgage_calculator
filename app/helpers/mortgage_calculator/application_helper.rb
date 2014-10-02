607aced028abf80c90c05f40f318d683568c0d16
Mark Connell <mark@markconnell.co.uk>
2014-10-02 09:11:10 +0100

    add correct page title and description meta tags for the 3 calculators

--- app/helpers/mortgage_calculator/application_helper.rb
+++ app/helpers/mortgage_calculator/application_helper.rb
@@ -3,6 +3,14 @@ module MortgageCalculator
     # Feedback does not appear to inject itself
     include Mas::Feedback::ApplicationHelper
 
+    def calculator_name
+      {
+        'affordabilities' => 'affordability',
+        'stamp_duties'    => 'stamp_duty',
+        'repayments'      => 'mortgage_calculator'
+      }[controller_name]
+    end
+
     def zendesk_stamp_duty_config
       @zendesk_stamp_duty_config ||= MortgageCalculator.feedback_config[:stamp_duty]
     end
