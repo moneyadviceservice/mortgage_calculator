41ef8715e81ee8871c487dbcfc44f785047ab440
Dan Munckton <dan@cultivatehq.com>
2014-12-03 16:37:43 +0000

    Present full numeric keypad (with decimal point) for the rate field

--- app/assets/javascripts/mortgage_calculator/angular/directives/percentage.js
+++ app/assets/javascripts/mortgage_calculator/angular/directives/percentage.js
@@ -2,17 +2,17 @@
 
 App.directive('percentage', function () {
   var linker = function (scope, element, attrs, ctrl) {
-    element.autoNumeric('init');
-
     ctrl.$parsers.unshift(function (viewValue) {
       if (viewValue) {
         return parseFloat(viewValue.replace(/[^\d|\-+|\.+]/g, ''));
       }
     });
 
-    ctrl.$formatters.unshift(function (a) {
-     if (a)
-      return element.autoNumeric('set', a).val();
+    ctrl.$formatters.unshift(function (valueString) {
+      if (valueString) {
+        // Cannot use autoNumeric here, it doesn't work with input[type=number]
+        return parseFloat(valueString).toFixed(2);
+      }
     });
   };
 
