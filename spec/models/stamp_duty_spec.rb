0701419181417674b12ec9f2f0798c1ad2a8b39d
Phil Lee <asmega@ph-lee.com>
2014-12-04 17:20:41 +0000

    wip new stamp duty model

--- spec/models/stamp_duty_spec.rb
+++ spec/models/stamp_duty_spec.rb
@@ -24,7 +24,6 @@ describe MortgageCalculator::StampDuty do
     context 'when house price is 0' do
       let(:price) { 0 }
 
-      its(:percentage_rate) { should be_zero }
       its(:tax_due) { should be_zero }
       its(:total_due) { should be_zero }
     end
@@ -32,91 +31,43 @@ describe MortgageCalculator::StampDuty do
     context 'when house price is 125000' do
       let(:price) { 125000 }
 
-      its(:percentage_rate) { should be_zero }
       its(:tax_due) { should be_zero }
       its(:total_due) { should == 125000 }
     end
 
-    context 'when house price is 125000.01' do
-      let(:price) { 125000.01 }
+    context 'when house price is 185000.00' do
+      let(:price) { BigDecimal('185000.00') }
 
-      its(:percentage_rate) { should == 1 }
-      its(:tax_due) { should == 1250.00 }
-      its(:total_due) { should == 126250.01 }
+      its(:tax_due) { should == BigDecimal('1200.00') }
+      its(:total_due) { should == 186200.00 }
     end
 
-    context 'when house price is 250000' do
-      let(:price) { 250000 }
+    context 'when house price is 275000' do
+      let(:price) { 275000 }
 
-      its(:percentage_rate) { should == 1 }
-      its(:tax_due) { should == 2500.00 }
-      its(:total_due) { should == 252500 }
+      its(:tax_due) { should == 3750.00 }
+      its(:total_due) { should == 278750 }
     end
 
-    context 'when house price is 250000.01' do
-      let(:price) { 250000.01 }
+    context 'when house price is 510000.00' do
+      let(:price) { 510000.00 }
 
-      its(:percentage_rate) { should == 3 }
-      its(:tax_due) { should == 7500.00 }
-      its(:total_due) { should == 257500.01 }
+      its(:tax_due) { should == 15500.00 }
+      its(:total_due) { should == 525500.00 }
     end
 
-    context 'when house price is 500000' do
-      let(:price) { 500000 }
+    context 'when house price is 937500' do
+      let(:price) { 937500 }
 
-      its(:percentage_rate) { should == 3 }
-      its(:tax_due) { should == 15000 }
-      its(:total_due) { should == 515000 }
+      its(:tax_due) { should == 37500 }
+      its(:total_due) { should == 975000 }
     end
 
-    context 'when house price is 500000.01' do
-      let(:price) { 500000.01 }
+    context 'when house price is 2100000.00' do
+      let(:price) { 2100000.00 }
 
-      its(:percentage_rate) { should == 4 }
-      its(:tax_due) { should == 20000 }
-      its(:total_due) { should == 520000.01 }
-    end
-
-    context 'when house price is 1000000' do
-      let(:price) { 1000000 }
-
-      its(:percentage_rate) { should == 4 }
-      its(:tax_due) { should == 40000 }
-      its(:total_due) { should == 1040000 }
-    end
-
-    context 'when house price is 1000000.01' do
-      let(:price) { 1000000.01 }
-
-      its(:percentage_rate) { should == 5 }
-      its(:tax_due) { should == 50000 }
-      its(:total_due) { should == 1050000.01 }
-    end
-
-    context 'when house price is 2000000' do
-      let(:price) { 2000000 }
-
-      its(:percentage_rate) { should == 5 }
-      its(:tax_due) { should == 100000 }
-      its(:total_due) { should == 2100000 }
-    end
-
-    context 'when house price is 2000000.01' do
-      let(:price) { 2000000.01 }
-
-      its(:percentage_rate) { should == 7 }
-      its(:tax_due) { should == 140000 }
-      # On upgrading to Ruby 2.1.2 the expected value needs to be specified
-      # as a BigDecimal or this lone test fails.
-      its(:total_due) { should == BigDecimal('2140000.01') }
-    end
-
-    context 'when house price is 3000000' do
-      let(:price) { 3000000 }
-
-      its(:percentage_rate) { should == 7 }
-      its(:tax_due) { should == 210000 }
-      its(:total_due) { should == 3210000 }
+      its(:tax_due) { should == 165750 }
+      its(:total_due) { should == 2265750.00 }
     end
   end
 end
