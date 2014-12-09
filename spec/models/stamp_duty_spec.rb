8acc6a0402de842cca5c25f8bd546873bfdd2f6e
Phil Lee <asmega@ph-lee.com>
2014-12-09 11:37:30 +0000

    update to use new rspec syntax

--- spec/models/stamp_duty_spec.rb
+++ spec/models/stamp_duty_spec.rb
@@ -24,57 +24,57 @@ describe MortgageCalculator::StampDuty do
     context 'when house price is 0' do
       let(:price) { 0 }
 
-      its(:tax_due) { should be_zero }
-      its(:percentage_tax) { should be_zero }
-      its(:total_due) { should be_zero }
+      its(:tax_due) { is_expected.to be_zero }
+      its(:percentage_tax) { is_expected.to be_zero }
+      its(:total_due) { is_expected.to be_zero }
     end
 
     context 'when house price is 125000' do
       let(:price) { 125000 }
 
-      its(:tax_due) { should be_zero }
-      its(:percentage_tax) { should be_zero }
-      its(:total_due) { should == 125000 }
+      its(:tax_due) { is_expected.to be_zero }
+      its(:percentage_tax) { is_expected.to be_zero }
+      its(:total_due) { is_expected.to eql(125000) }
     end
 
     context 'when house price is 185000.00' do
-      let(:price) { BigDecimal('185000.00') }
+      let(:price) { 185000 }
 
-      its(:tax_due) { should == BigDecimal('1200.00') }
-      its(:percentage_tax) { should be_within(0.1).of(0.7) }
-      its(:total_due) { should == 186200.00 }
+      its(:tax_due) { is_expected.to eql(1200.00) }
+      its(:percentage_tax) { is_expected.to be_within(0.1).of(0.7) }
+      its(:total_due) { is_expected.to eql(186200.00) }
     end
 
     context 'when house price is 275000' do
       let(:price) { 275000 }
 
-      its(:tax_due) { should == 3750.00 }
-      its(:percentage_tax) { should be_within(0.1).of(1.4) }
-      its(:total_due) { should == 278750 }
+      its(:tax_due) { is_expected.to eql(3750.00) }
+      its(:percentage_tax) { is_expected.to be_within(0.1).of(1.4) }
+      its(:total_due) { is_expected.to eql(278750) }
     end
 
     context 'when house price is 510000.00' do
       let(:price) { 510000.00 }
 
-      its(:tax_due) { should == 15500.00 }
-      its(:percentage_tax) { should be_within(0.1).of(3) }
-      its(:total_due) { should == 525500.00 }
+      its(:tax_due) { is_expected.to eql(15500.00) }
+      its(:percentage_tax) { is_expected.to be_within(0.1).of(3) }
+      its(:total_due) { is_expected.to eql(525500.00) }
     end
 
     context 'when house price is 937500' do
       let(:price) { 937500 }
 
-      its(:tax_due) { should == 37500 }
-      its(:percentage_tax) { should be_within(0.1).of(4) }
-      its(:total_due) { should == 975000 }
+      its(:tax_due) { is_expected.to eql(37500) }
+      its(:percentage_tax) { is_expected.to be_within(0.1).of(4) }
+      its(:total_due) { is_expected.to eql(975000) }
     end
 
     context 'when house price is 2100000.00' do
       let(:price) { 2100000.00 }
 
-      its(:tax_due) { should == 165750 }
-      its(:percentage_tax) { should be_within(0.1).of(7.9) }
-      its(:total_due) { should == 2265750.00 }
+      its(:tax_due) { is_expected.to eql(165750) }
+      its(:percentage_tax) { is_expected.to be_within(0.1).of(7.9) }
+      its(:total_due) { is_expected.to eql(2265750.00) }
     end
   end
 end
