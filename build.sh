9e9094548fe6638721fd9528967824c9db57ba04
pmcostello <pmcostello@hotmail.co.uk>
2016-03-10 14:38:56 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,20 @@ set -e -x
 export RAILS_ENV=build
 export BUNDLE_WITHOUT="development:test"
 
-CI_PIPELINE_COUNTER=${GO_PIPELINE_LABEL-0}
-CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
+#CI_PIPELINE_COUNTER=${GO_PIPELINE_LABEL-0}
+#CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
+
+version_number=${GO_PIPELINE_LABEL-0}
+revision=`git rev-parse HEAD`
+build_date=`date +'%Y-%m-%d %H:%M %z'`
+
+cat > public/version <<EOT
+{
+  "version":"$version_number",
+  "buildDate":"$build_date",
+  "gitRevision":"$revision"
+}
+EOT
 
 bundle install
 
