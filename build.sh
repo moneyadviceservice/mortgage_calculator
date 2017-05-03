74471a18b9e1797d68c678f748b8dd426db68406
John Player <johnplayer1982@msn.com>
2017-05-03 11:04:21 +0100

    Clearing bower cache, removing bower components and then install

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ CI_PIPELINE_COUNTER=${GO_PIPELINE_LABEL:-0}
 CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER:-0}
 
 bundle install
+
+bower cache clean
+[ -d vendor/assets/bower_components ] && rm -r vendor/assets/bower_components
 bundle exec bowndler install
 
 rake app:gem:build
