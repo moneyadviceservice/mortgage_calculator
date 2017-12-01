0afad8098898b599d2ad2c9776cab85e33989731
John Player <johnplayer1982@msn.com>
2017-12-01 10:23:24 +0000

    Bumps major version of the tool, as its a change to the way the tool works

--- lib/mortgage_calculator/version.rb
+++ lib/mortgage_calculator/version.rb
@@ -1,8 +1,8 @@
 module MortgageCalculator
   module Version
-    MAJOR = 1
-    MINOR = 11
-    PATCH = 1
+    MAJOR = 2
+    MINOR = 0
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
