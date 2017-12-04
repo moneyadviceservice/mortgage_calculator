60a1904629a3250e7e6aba6d7a28066633d2ea06
Margo Urey <margo@margonline.co.uk>
2017-12-04 17:08:14 +0000

    use defined constants in translation files

--- spec/helpers/stamp_duties_helper_spec.rb
+++ spec/helpers/stamp_duties_helper_spec.rb
@@ -5,14 +5,20 @@ module MortgageCalculator
     describe '#band' do
       context 'when there is a lower and an upper limit' do
         it 'returns a currency range with both limits formatted' do
-          expect(band(0, 125000) ).to eq("£0 - £125,000")
+          expect(band(0, 125000)).to eq('£0 - £125,000')
         end
       end
 
       context 'for the maximum band' do
         it 'returns text for the maximum band' do
-          expect(band(1500001, nil) ).to eq("Over £1.5 million")
+          expect(band(1500001, nil)).to eq('Over £1.5 million')
         end
+      end
+    end
+
+    describe '#second_home_threshold' do
+      it 'returns the second home threshold formatted with a currency sign' do
+        expect(second_home_threshold).to eq('£40,000')
       end
     end
   end
