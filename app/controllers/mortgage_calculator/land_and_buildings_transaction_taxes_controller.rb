e27d86b5226b060b4610f654c82ce762e411ed1a
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-03-19 15:19:42 +0000

    Remove duplicated views on scotland and wales calculators
    The views were duplicated for the lbtt for Scotland and
    ltt for Wales, so using Rails feature "Single template inheritance"
    I organised the controller and views in the following manner:
    
    Each calculator controller will have two methods:
    
    * I18n key - corresponding to the key to look on the translations for
    each calculator.
    * The form path - each calculator has your own submit form url.
    
    And the show view is organised as:
    
     - show.html.erb
        - calculator_form
        - links_to_other_tools
        - results_recalculation_form

--- app/controllers/mortgage_calculator/land_and_buildings_transaction_taxes_controller.rb
+++ app/controllers/mortgage_calculator/land_and_buildings_transaction_taxes_controller.rb
@@ -3,5 +3,15 @@ module MortgageCalculator
     def calculator
       MortgageCalculator::LandAndBuildingsTransactionTax
     end
+
+    def i18n_locale_namespace
+      'land_and_buildings_transaction_tax'
+    end
+    helper_method :i18n_locale_namespace
+
+    def calculator_form_path
+      land_and_buildings_transaction_tax_path
+    end
+    helper_method :calculator_form_path
   end
 end
