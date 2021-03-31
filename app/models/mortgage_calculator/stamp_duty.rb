e2772b0ae0ec4b1270917bd350067d0621727f0e
thenapking <tom.giles@gmail.com>
2021-03-31 10:22:45 +0100

    Alter FTB tables

--- app/models/mortgage_calculator/stamp_duty.rb
+++ app/models/mortgage_calculator/stamp_duty.rb
@@ -9,9 +9,13 @@ module MortgageCalculator
     FIRST_TIME_BUYER_BANDS = { phase_1: [{ threshold: 500_000, rate: 0 },
                                          { threshold: 500_001, rate: 5 }],
                                phase_2: [{ threshold: 300_000, rate: 0 },
-                                         { threshold: 500_000, rate: 5 }],
+                                         { threshold: 500_000, rate: 5 },
+                                         { threshold: 1_500_000, rate: 10 },
+                                         { threshold: nil, rate: 12 }],
                                phase_3: [{ threshold: 300_000, rate: 0 },
-                                         { threshold: 500_000, rate: 5 }] }.freeze
+                                         { threshold: 500_000, rate: 5 },
+                                         { threshold: 1_500_000, rate: 10 },
+                                         { threshold: nil, rate: 12 }] }.freeze
 
     STANDARD_BANDS = { phase_1: [{ threshold: 500_000, rate: 0 },
                                  { threshold: 925_000, rate: 5 },
