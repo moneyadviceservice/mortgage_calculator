11c850a207e3a75382023514ea23dbcf072991ed
Stephen Rycyk <stephen.rycyk@maps.org.uk>
2021-07-26 11:50:05 +0100

    Rspec phase test style updates

--- spec/helpers/phase_helper_spec.rb
+++ spec/helpers/phase_helper_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 module MortgageCalculator
   describe PhaseHelper do
-    completion_dates = [ Date.new(2021, 7, 1), Date.new(2021, 9, 30), Date.new(2021, 10, 1) ]
+    completion_dates = [Date.new(2021, 7, 1), Date.new(2021, 9, 30), Date.new(2021, 10, 1)]
     completion_phases = %i[phase_2 phase_2 phase_3]
     completion_expectations = completion_dates.zip(completion_phases)
 
-    context '#phase' do
+    context 'When calling #phase' do
       completion_expectations.each do |(date, phase)|
         it "returns phase: #{phase} on #{date}" do
           allow(helper).to receive(:completion_date).and_return(date)
