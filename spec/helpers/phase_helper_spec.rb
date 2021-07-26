c125381333c7cf545acb406c026ec34d18748b42
Stephen Rycyk <stephen.rycyk@maps.org.uk>
2021-07-26 11:36:11 +0100

    Rspec phase test swap constants for local vars

--- spec/helpers/phase_helper_spec.rb
+++ spec/helpers/phase_helper_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 module MortgageCalculator
   describe PhaseHelper do
-    COMPLETION_DATES = [ Date.new(2021, 7, 1), Date.new(2021, 9, 30), Date.new(2021, 10, 1) ]
-    COMPLETION_PHASES = %i[phase_2 phase_2 phase_3]
-    COMPLETION_EXPECTATIONS = COMPLETION_DATES.zip(COMPLETION_PHASES)
+    completion_dates = [ Date.new(2021, 7, 1), Date.new(2021, 9, 30), Date.new(2021, 10, 1) ]
+    completion_phases = %i[phase_2 phase_2 phase_3]
+    completion_expectations = completion_dates.zip(completion_phases)
 
     context '#phase' do
-      COMPLETION_EXPECTATIONS.each do |(date, phase)|
+      completion_expectations.each do |(date, phase)|
         it "returns phase: #{phase} on #{date}" do
           allow(helper).to receive(:completion_date).and_return(date)
 
